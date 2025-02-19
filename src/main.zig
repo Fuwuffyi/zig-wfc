a3da16c37b9502103499612bbd3740aeeafb8c43
Fuwuffy <palazzini21@gmail.com>
2025-02-19 13:10:14 +0100

    Make base tile extracting code

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,22 @@
 const std = @import("std");
-const zigimg = @import("zigimg");
+const Tile = @import("tile.zig").Tile;
 
 pub fn main() !void {
     // Create allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-    // Read image
-    const image = try zigimg.Image.fromFilePath(allocator, "test.png");
-    defer image.deinit();
+    // Get the tiles
+    const tile_size: u8 = 3;
+    const tiles = try Tile.generate_tiles(&allocator, "test.png", tile_size);
+    defer allocator.free(tiles);
+    // Read the tile data
+    for (tiles, 0..) |tile, i| {
+        defer tile.deinit(&allocator);
+        std.debug.print("\nTile: {}\n", .{i});
+        for (tile.colors, 0..) |col, ci| {
+            if (ci % tile_size == 0) std.debug.print("\n", .{});
+            std.debug.print("\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m   \x1b[0m", .{ col.r, col.g, col.b, col.r, col.g, col.b });
+        }
+    }
 }
---
+++ src/tile.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const zigimg = @import("zigimg");
+
+const Color = struct { r: u8, g: u8, b: u8 };
+
+pub const Tile = struct {
+    colors: []Color,
+    dim: u8,
+
+    pub fn generate_tiles(allocator: *const std.mem.Allocator, image_file: []const u8, tile_size: u8) ![]Tile {
+        // Read image
+        var image = try zigimg.Image.fromFilePath(allocator.*, image_file);
+        defer image.deinit();
+        // Create the tiles to return
+        const tiles: []Tile = try allocator.alloc(Tile, image.width * image.height);
+        errdefer allocator.free(tiles);
+        // Load the pixels to a local buffer
+        var pixels = try allocator.alloc(Color, image.width * image.height);
+        defer allocator.free(pixels);
+        var color_it = image.iterator();
+        var idx: usize = 0;
+        while (color_it.next()) |pixel| : (idx += 1) {
+            pixels[idx] = .{ .r = @intFromFloat(pixel.r * 255), .g = @intFromFloat(pixel.g * 255), .b = @intFromFloat(pixel.b * 255) };
+        }
+        // Generate tile colors
+        for (tiles, 0..) |*tile, i| {
+            // Allocate new colors for the tile
+            const colors = try allocator.alloc(Color, tile_size * tile_size);
+            errdefer allocator.free(colors);
+            // Read the colors from the pixels array
+            var colors_idx: usize = 0;
+            for (0..tile_size) |dy| {
+                const y: usize = (i / tile_size + dy) % image.height;
+                for (0..tile_size) |dx| {
+                    const x: usize = (i % tile_size + dx) % image.width;
+                    const pixels_idx: usize = y * image.width + x;
+                    const color = &pixels[pixels_idx];
+                    colors[colors_idx] = .{ .r = color.r, .g = color.g, .b = color.b };
+                    colors_idx += 1;
+                }
+            }
+            // Finalize the new tile
+            tile.* = .{ .colors = colors, .dim = tile_size };
+        }
+        return tiles;
+    }
+
+    pub fn deinit(self: *const Tile, allocator: *const std.mem.Allocator) void {
+        allocator.free(self.colors);
+    }
+};
