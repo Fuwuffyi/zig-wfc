1a0c519b2f853e40743887b530c2bfa84ee35b3b
Fuwuffy <palazzini21@gmail.com>
2025-02-21 15:13:33 +0100

    Add collapsing part of algorithm

--- src/main.zig
+++ src/main.zig
@@ -17,25 +17,27 @@ pub fn main() !void {
     const tileset: TileSet = try TileSet.init(&allocator, "samples/Lake.png", tile_size);
     defer tileset.deinit(&allocator);
     // Create a map
-    const wfc_map: WfcMap = try WfcMap.init(&allocator, &tileset, terminal.dimensions.width, terminal.dimensions.height);
+    var wfc_map: WfcMap = try WfcMap.init(&allocator, &tileset, terminal.dimensions.width, terminal.dimensions.height);
     defer wfc_map.deinit(&allocator);
-    //Print wfc state
-    const tile_center_idx: usize = tile_size * tile_size / 2;
-    for (wfc_map.cells, 0..) |*cell, i| {
-        const x: u32 = @as(u32, @intCast(i)) % terminal.dimensions.width;
-        const y: u32 = @as(u32, @intCast(i)) / terminal.dimensions.width;
-        // Calculate average color
-        var total_entropy: u32 = 0;
-        var sum_r: u32 = 0;
-        var sum_g: u32 = 0;
-        var sum_b: u32 = 0;
-        for (cell.items) |idx| {
-            total_entropy += tileset.tiles[idx].freq;
-            sum_r += tileset.tiles[idx].colors[tile_center_idx].r * tileset.tiles[idx].freq;
-            sum_g += tileset.tiles[idx].colors[tile_center_idx].g * tileset.tiles[idx].freq;
-            sum_b += tileset.tiles[idx].colors[tile_center_idx].b * tileset.tiles[idx].freq;
+    // Print wfc state
+    while (!try wfc_map.step(&allocator)) {
+        const tile_center_idx: usize = tile_size * tile_size / 2;
+        for (wfc_map.cells, 0..) |*cell, i| {
+            const x: u32 = @as(u32, @intCast(i)) % terminal.dimensions.width;
+            const y: u32 = @as(u32, @intCast(i)) / terminal.dimensions.width;
+            // Calculate average color
+            var total_entropy: u32 = 0;
+            var sum_r: u32 = 0;
+            var sum_g: u32 = 0;
+            var sum_b: u32 = 0;
+            for (cell.items) |idx| {
+                total_entropy += tileset.tiles[idx].freq;
+                sum_r += tileset.tiles[idx].colors[tile_center_idx].r * tileset.tiles[idx].freq;
+                sum_g += tileset.tiles[idx].colors[tile_center_idx].g * tileset.tiles[idx].freq;
+                sum_b += tileset.tiles[idx].colors[tile_center_idx].b * tileset.tiles[idx].freq;
+            }
+            terminal.setPixel(x, y, .{ .r = @intCast(sum_r / total_entropy), .g = @intCast(sum_g / total_entropy), .b = @intCast(sum_b / total_entropy) });
         }
-        terminal.setPixel(x, y, .{ .r = @intCast(sum_r / total_entropy), .g = @intCast(sum_g / total_entropy), .b = @intCast(sum_b / total_entropy) });
+        try terminal.draw();
     }
-    try terminal.draw();
 }
--- src/wfcmap.zig
+++ src/wfcmap.zig
@@ -2,16 +2,20 @@ const std = @import("std");
 const Tile = @import("tile.zig").Tile;
 const TileSet = @import("tileset.zig").TileSet;
 
+// TODO: Hardcoded seed...
+var random_generator: std.Random.Xoshiro256 = std.Random.DefaultPrng.init(0);
+const random: std.Random = random_generator.random();
+
 pub const WfcMap = struct {
-    cells: []std.ArrayList(usize),
+    cells: []std.ArrayList(u32),
     tileset: *const TileSet,
 
     pub fn init(allocator: *const std.mem.Allocator, tileset: *const TileSet, width: usize, height: usize) !@This() {
-        const cells: []std.ArrayList(usize) = try allocator.alloc(std.ArrayList(usize), width * height);
+        const cells: []std.ArrayList(u32) = try allocator.alloc(std.ArrayList(u32), width * height);
         for (cells) |*cell| {
-            cell.* = std.ArrayList(usize).init(allocator.*);
+            cell.* = std.ArrayList(u32).init(allocator.*);
             for (0..tileset.tiles.len) |i| {
-                try cell.append(i);
+                try cell.append(@intCast(i));
             }
         }
         return .{ .cells = cells, .tileset = tileset };
@@ -24,5 +28,38 @@ pub const WfcMap = struct {
         allocator.free(self.cells);
     }
 
-    // TODO: Implement WFC algorithm
+    pub fn step(self: *@This(), allocator: *const std.mem.Allocator) !bool {
+        // Get lowest entropy elements
+        var lowest_entropy: u32 = std.math.maxInt(u32);
+        var lowest_entropy_cells: std.ArrayList(usize) = std.ArrayList(usize).init(allocator.*);
+        defer lowest_entropy_cells.deinit();
+        for (self.cells, 0..) |*cell, m_idx| {
+            // Skip already collapsed elements
+            if (cell.items.len <= 1) continue;
+            // Calculate entropy
+            var cell_entropy: u32 = 0;
+            for (cell.items) |c_idx| {
+                cell_entropy += self.tileset.tiles[c_idx].freq;
+            }
+            // Clear list if new entropy record found
+            if (cell_entropy < lowest_entropy) {
+                lowest_entropy_cells.clearAndFree();
+                lowest_entropy = cell_entropy;
+            }
+            // Add entropy cell if it has same entropy as best
+            if (cell_entropy == lowest_entropy) {
+                try lowest_entropy_cells.append(m_idx);
+            }
+        }
+        // If the lowest entropy does not change, we have collapsed all cells
+        if (lowest_entropy == std.math.maxInt(u32)) return true;
+        // Collapse a random lowest entropy element
+        const rand_index: usize = lowest_entropy_cells.items[random.int(usize) % lowest_entropy_cells.items.len];
+        var rand_cell: *std.ArrayList(u32) = &self.cells[rand_index];
+        const random_tile: u32 = rand_cell.items[random.int(usize) % rand_cell.items.len];
+        rand_cell.clearAndFree();
+        try rand_cell.append(random_tile);
+        // Not finished collapsing everything yet
+        return false;
+    }
 };
