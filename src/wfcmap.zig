1c81d190c7d5efcc352ddf074aa7ca6afea68204
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2025-02-24 21:57:20 +0100

    Make wfc map a 2D array instead of a single array

--- src/wfcmap.zig
+++ src/wfcmap.zig
@@ -6,37 +6,44 @@ const TileSet = @import("tileset.zig").TileSet;
 pub const WfcError = error{Contradiction};
 
 // TODO: Hardcoded seed...
-var random_generator: std.Random.Xoshiro256 = std.Random.DefaultPrng.init(42);
+var random_generator: std.Random.Xoshiro256 = std.Random.DefaultPrng.init(0);
 const random: std.Random = random_generator.random();
 
 pub const WfcMap = struct {
+    const Pair = std.meta.Tuple(&.{ i64, i64 });
     const Cell = struct {
         possible: std.DynamicBitSet,
         entropy: u32,
     };
 
-    cells: []Cell,
+    cells: [][]Cell,
     width: u32,
     height: u32,
     tileset: *const TileSet,
 
     pub fn init(allocator: *const std.mem.Allocator, tileset: *const TileSet, width: u32, height: u32) !@This() {
         // Create the map
-        var map: WfcMap = .{ .cells = try allocator.alloc(Cell, width * height), .width = width, .height = height, .tileset = tileset };
+        var map: WfcMap = .{ .cells = try allocator.alloc([]Cell, height), .width = width, .height = height, .tileset = tileset };
         const num_tiles: usize = tileset.tiles.len;
-        for (map.cells) |*cell| {
-            cell.* = .{
-                .possible = try std.DynamicBitSet.initEmpty(allocator.*, num_tiles),
-                .entropy = 0,
-            };
+        for (map.cells) |*row| {
+            row.* = try allocator.alloc(Cell, width);
+            for (row.*) |*cell| {
+                cell.* = .{
+                    .possible = try std.DynamicBitSet.initEmpty(allocator.*, num_tiles),
+                    .entropy = 0,
+                };
+            }
         }
         map.reset();
         return map;
     }
 
     pub fn deinit(self: *const @This(), allocator: *const std.mem.Allocator) void {
-        for (self.cells) |*cell| {
-            cell.possible.deinit();
+        for (self.cells) |*row| {
+            defer allocator.free(row.*);
+            for (row.*) |*cell| {
+                cell.possible.deinit();
+            }
         }
         allocator.free(self.cells);
     }
@@ -44,41 +51,46 @@ pub const WfcMap = struct {
     pub fn reset(self: *@This()) void {
         // Reset all cells
         const num_tiles: usize = self.tileset.tiles.len;
-        for (self.cells) |*cell| {
-            cell.possible.setRangeValue(.{ .start = 0, .end = num_tiles }, true);
-            cell.*.entropy = calculate_cell_entropy(self.tileset, cell.possible);
+        for (self.cells) |*row| {
+            for (row.*) |*cell| {
+                cell.possible.setRangeValue(.{ .start = 0, .end = num_tiles }, true);
+                cell.*.entropy = calculate_cell_entropy(self.tileset, cell.possible);
+            }
         }
     }
 
     pub fn step(self: *@This(), allocator: *const std.mem.Allocator) !bool {
         // Get lowest entropy elements
         var lowest_entropy: u32 = std.math.maxInt(u32);
-        var lowest_entropy_cells: std.ArrayList(usize) = std.ArrayList(usize).init(allocator.*);
+        var lowest_entropy_cells: std.ArrayList(Pair) = std.ArrayList(Pair).init(allocator.*);
         defer lowest_entropy_cells.deinit();
-        for (self.cells, 0..) |*cell, m_idx| {
-            // Skip already collapsed elements
-            if (cell.possible.count() <= 1) continue;
-            // Clear list if new entropy record found
-            if (cell.entropy < lowest_entropy) {
-                lowest_entropy_cells.clearAndFree();
-                lowest_entropy = cell.entropy;
-            }
-            // Add entropy cell if it has same entropy as best
-            if (cell.entropy == lowest_entropy) {
-                try lowest_entropy_cells.append(m_idx);
+        for (self.cells, 0..) |*row, row_idx| {
+            for (row.*, 0..) |*cell, col_idx| {
+                const m_idx: Pair = .{ @intCast(row_idx), @intCast(col_idx) };
+                // Skip already collapsed elements
+                if (cell.possible.count() <= 1) continue;
+                // Clear list if new entropy record found
+                if (cell.entropy < lowest_entropy) {
+                    lowest_entropy_cells.clearAndFree();
+                    lowest_entropy = cell.entropy;
+                }
+                // Add entropy cell if it has same entropy as best
+                if (cell.entropy == lowest_entropy) {
+                    try lowest_entropy_cells.append(m_idx);
+                }
             }
         }
         // If the lowest entropy does not change, we have collapsed all cells
         if (lowest_entropy == std.math.maxInt(u32)) return true;
         // Collapse a random lowest entropy element
-        const rand_index: usize = lowest_entropy_cells.items[random.int(usize) % lowest_entropy_cells.items.len];
-        var rand_cell = &self.cells[rand_index];
+        const rand_index: Pair = lowest_entropy_cells.items[random.int(usize) % lowest_entropy_cells.items.len];
+        var rand_cell: *Cell = &self.cells[@intCast(rand_index.@"0")][@intCast(rand_index.@"1")];
         const random_tile: u32 = select_tile_by_frequency(self.tileset, &rand_cell.possible);
         rand_cell.possible.deinit();
         rand_cell.possible = try std.DynamicBitSet.initEmpty(allocator.*, self.tileset.tiles.len);
         rand_cell.possible.set(random_tile);
         // Propagate collapse to adjacent tiles
-        try self.update_neighbors(allocator, rand_index);
+        try self.update_neighbors(allocator, &rand_index);
         // Not finished collapsing everything yet
         return false;
     }
@@ -108,43 +120,29 @@ pub const WfcMap = struct {
         unreachable;
     }
 
-    fn update_neighbors(self: *@This(), allocator: *const std.mem.Allocator, starting_index: usize) !void {
+    fn update_neighbors(self: *@This(), allocator: *const std.mem.Allocator, starting_index: *const Pair) !void {
         // Create queue of cells to update
-        var cell_queue: std.ArrayList(usize) = std.ArrayList(usize).init(allocator.*);
+        var cell_queue: std.ArrayList(Pair) = std.ArrayList(Pair).init(allocator.*);
         defer cell_queue.deinit();
-        try cell_queue.append(starting_index);
+        try cell_queue.append(starting_index.*);
         // Update cells for as long as they are in the queue
         while (cell_queue.popOrNull()) |current_index| {
             // Get current cell data
-            const current_cell = &self.cells[current_index];
-            const current_x: u32 = @as(u32, @intCast(current_index % self.width));
-            const current_y: u32 = @as(u32, @intCast(current_index / self.width));
+            const current_cell: *const Cell = &self.cells[@intCast(current_index.@"0")][@intCast(current_index.@"1")];
             // Loop over the directions
             for (std.enums.values(Direction)) |dir| {
                 // Get neighbor cell's position
-                var nx: u32 = current_x;
-                var ny: u32 = current_y;
-                switch (dir) {
-                    .up => {
-                        if (current_y == 0) continue;
-                        ny -= 1;
-                    },
-                    .down => {
-                        if (current_y >= self.height - 1) continue;
-                        ny += 1;
-                    },
-                    .left => {
-                        if (current_x == 0) continue;
-                        nx -= 1;
-                    },
-                    .right => {
-                        if (current_x >= self.width - 1) continue;
-                        nx += 1;
-                    },
-                }
+                const neighbor_position: Pair = switch (dir) {
+                    .up => .{ .@"0" = current_index.@"0" - 1, .@"1" = current_index.@"1" },
+                    .down => .{ .@"0" = current_index.@"0" + 1, .@"1" = current_index.@"1" },
+                    .left => .{ .@"0" = current_index.@"0", .@"1" = current_index.@"1" - 1 },
+                    .right => .{ .@"0" = current_index.@"0", .@"1" = current_index.@"1" + 1 },
+                };
+                // Skip if out of bounds
+                if (neighbor_position.@"0" < 0 or neighbor_position.@"0" >= @as(usize, @intCast(self.height))) continue;
+                if (neighbor_position.@"1" < 0 or neighbor_position.@"1" >= @as(usize, @intCast(self.width))) continue;
                 // Calculate the neighbor's index
-                const neighbor_index: usize = @as(usize, @intCast(ny * self.width + nx));
-                const neighbor_cell = &self.cells[neighbor_index];
+                const neighbor_cell: *Cell = &self.cells[@intCast(neighbor_position.@"0")][@intCast(neighbor_position.@"1")];
                 // Skip the neighbor if already collapsed
                 if (neighbor_cell.possible.count() <= 1) continue;
                 // Get map of allowed indices based on the direction
@@ -161,7 +159,7 @@ pub const WfcMap = struct {
                 if (new_count == 0) return error.Contradiction;
                 if (new_count < prev_count) {
                     neighbor_cell.entropy = calculate_cell_entropy(self.tileset, neighbor_cell.possible);
-                    try cell_queue.append(neighbor_index);
+                    try cell_queue.append(neighbor_position);
                 }
             }
         }
